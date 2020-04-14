4b516ff52295128b9b16e45f4eab7ef788e58c54
Travis CI User <travis@example.org>
2020-04-14 22:05:50 +0000

    Updates

---
+++ component---src-pages-404-en-js-41535c0eb6d0b94f18b6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{sNZU:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Bl7J"),s=n("vrFN");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(s.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-en-js-41535c0eb6d0b94f18b6.js.map
