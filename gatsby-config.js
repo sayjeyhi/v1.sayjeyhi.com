2d8d15884530f04317859532f47c1abbc46cbe52
sayjeyhi <sayjeyhi@gmail.com>
2021-06-19 19:22:56 +0430

    use gtag instead of ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,10 +15,14 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-MG7Z4SE26B",
-        head: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-MG7Z4SE26B"],
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+        },
       },
     },
     {
